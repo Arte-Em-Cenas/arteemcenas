bd1011ae5cea946480bbe219012dd3d0055ed9cd
Arte-Em-Cenas <grupoteatralarteemcenas@gmail.com>
2025-03-30 01:35:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -63,3 +63,12 @@ function openModal(imgSrc, imgDesc) {
   document.getElementById("modalDesc").textContent = imgDesc
   document.getElementById("imageModal").style.display = "block";
 }
+
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
